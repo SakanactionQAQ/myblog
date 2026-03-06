d8bd72d05494665316c064884b02e9e1b414f166
zewei <ai-2246111878@stu.xjtu.edu.cn>
2026-03-06 13:18:53 +0800

    rebuild site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,9 @@ import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://learning-up-up.github.io",
   output: 'static',
-  base: '/myblog/',
+  base: "/myblog/",
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
